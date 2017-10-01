8362f09f7c1c9b3c645ab5ef1027752d4d055328
Scarletta Julia Yapfrine <scarletta@Scarlettas-MacBook-Pro.local>
2017-10-01 14:34:45 +0700

    update main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import weka.classifiers.C45;
 import weka.classifiers.Evaluation;
 import weka.classifiers.Id3TomMitchell;
 import weka.core.Instances;
+import weka.filters.ContinuousAttr;
+import weka.filters.Filter;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -21,15 +23,16 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
+        C45 c45 = new C45();
 
-        Instances data = ReadArff(String.valueOf("/root/Documents/weka-3-6-14/src/data/soybean.arff"));
+        Instances data = ReadArff("/Users/scarletta/Downloads/weka-3-6-14/data/iris.arff");
+        data.randomize(new java.util.Random(0));
+        data = c45.handleContinuousAttribute(data);
         data.randomize(new java.util.Random(0));
         int trainSize = (int) Math.round(data.numInstances() * 0.8);
         int testSize = data.numInstances() - trainSize;
         Instances train = new Instances(data, 0, trainSize);
         Instances test = new Instances(data, trainSize, testSize);
-
-        C45 c45 = new C45();
 
         c45.buildClassifier(train);
 
